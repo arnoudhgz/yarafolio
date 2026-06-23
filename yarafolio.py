8301722c6b4ac6db8bb094a36cd86689a4466361
Arnoud Beekman <a.beekman@x-interactive.nl>
2026-06-23 19:10:48 +0200

    fix(server): remove cache-control headers from dev server

--- yarafolio.py
+++ yarafolio.py
@@ -88,11 +88,6 @@ class Handler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=ROOT, **kwargs)
 
-    def end_headers(self):
-        self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
-        self.send_header('Pragma', 'no-cache')
-        self.send_header('Expires', '0')
-        super().end_headers()
 
     def do_POST(self):
         if self.path == "/api/save":
